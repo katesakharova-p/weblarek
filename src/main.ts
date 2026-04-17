8a338bc18199e44609ef3450695745edeece491e
Kate Sakharova <kate.sakharova27@gmail.com>
2026-04-17 19:13:41 +0500

    12121212121

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,19 @@
-import './scss/styles.scss';
+import "./scss/styles.scss";
 
-import { EventEmitter } from './components/base/Events';
+import { EventEmitter } from "./components/base/Events";
 
-import { ProductsModel } from './components/models/ProductsModel';
-import { CartModel } from './components/models/CartModel';
+import { ProductsModel } from "./components/models/ProductsModel";
+import { CartModel } from "./components/models/CartModel";
+import { BuyerModel } from "./components/models/BuyerModel";
 
-import { WebLarekApi } from './components/api/WebLarekApi';
-import { Api } from './components/base/Api';
-import { API_URL } from './utils/constants';
+import { WebLarekApi } from "./components/api/WebLarekApi";
+import { Api } from "./components/base/Api";
+import { API_URL } from "./utils/constants";
 
-import { CatalogView } from './components/view/CatalogView';
-import { Modal } from './components/view/Modal';
+import { CatalogView } from "./components/view/CatalogView";
+import { Modal } from "./components/view/Modal";
 
-import { Presenter } from './components/Presenter';
+import { Presenter } from "./components/Presenter";
 
 // события
 const events = new EventEmitter();
@@ -23,10 +24,11 @@ const api = new WebLarekApi(new Api(API_URL));
 // модели
 const productsModel = new ProductsModel(events);
 const cartModel = new CartModel(events);
+const buyerModel = new BuyerModel(events); // ✅ ДОБАВИЛИ
 
 // view
-const catalogContainer = document.querySelector('.gallery') as HTMLElement;
-const modalElement = document.getElementById('modal-container') as HTMLElement;
+const catalogContainer = document.querySelector(".gallery") as HTMLElement;
+const modalElement = document.getElementById("modal-container") as HTMLElement;
 
 const catalogView = new CatalogView(catalogContainer);
 const modal = new Modal(modalElement, events);
@@ -36,8 +38,10 @@ new Presenter(
   events,
   productsModel,
   cartModel,
+  buyerModel,
   catalogView,
-  modal
+  modal,
+  api,
 );
 
 // загрузка данных
@@ -48,9 +52,9 @@ api
   })
   .catch(console.error);
 
-// обработка клика по иконке корзины
-const basketButton = document.querySelector('.header__basket') as HTMLElement;
+// корзина
+const basketButton = document.querySelector(".header__basket") as HTMLElement;
 
-basketButton.addEventListener('click', () => {
-  events.emit('basket:open');
+basketButton.addEventListener("click", () => {
+  events.emit("basket:open");
 });
